7674a7b24594f6637cfc51563bc930404c72e6fb
marvingregorio <marvingregorio@skydev.solutions>
2026-09-15 10:12:28 +0800

    :lipstick: apply zod validation in manual login

--- app/login-page/manual-login.tsx
+++ app/login-page/manual-login.tsx
@@ -1,22 +1,80 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
-import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+    username: z.string()
+        .min(8, "Username must be at least 8 characters."),
+    password: z.string()
+        .min(8, "Password must be at least 8 characters.")
+        .regex(/^[a-zA-Z0-9]+$/, "Password must be alphanumeric.")
+})
+
+type formSchemaType = z.infer<typeof formSchema>;
 
 export default function ManualLogin(){
+    const form = useForm<formSchemaType>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: ""
+        }
+    });
+
+    function onSubmit(data: formSchemaType){
+        console.log("submit succesfull")
+    }
+
     return (<>
-        <form>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
             <FieldSet>
                 <FieldLegend>Sign In</FieldLegend>
                 <FieldDescription>Welcome back, Sign in to your account</FieldDescription>
                 <FieldGroup>
-                    <Field>
-                        <FieldLabel htmlFor="username">Username</FieldLabel>
-                        <Input id="name" autoComplete="off" placeholder="Type your username here" required/>
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="password">Password</FieldLabel>
-                        <Input id="username" autoComplete="off" type="password" placeholder="Type your password here" required />
-                    </Field>
+                    <Controller 
+                        name="username"
+                        control={form.control}
+                        render={({field, fieldState}) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor="username">Username</FieldLabel>
+                                <Input
+                                    {...field}
+                                    aria-invalid={fieldState.invalid}
+                                    id="name"
+                                    autoComplete="off" 
+                                    placeholder="Type your username here"
+                                />
+                                {fieldState.invalid && (
+                                    <FieldError errors={[fieldState.error]} />
+                                )}
+                            </Field>
+                        )}
+                    />
+                    <Controller
+                        name="password"
+                        control={form.control}
+                        render={({field, fieldState}) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor="password">Password</FieldLabel>
+                                <Input 
+                                    {...field}
+                                    aria-invalid={fieldState.invalid}
+                                    id="password" 
+                                    autoComplete="off" 
+                                    type="password" 
+                                    placeholder="Type your password here" 
+                                />
+                                {fieldState.invalid && (
+                                    <FieldError errors={[fieldState.error]} />
+                                )}
+                            </Field>
+                        )}
+                    />
                     <Field>
                         <Button type="submit">Login</Button>
                     </Field>
--- app/login-page/page.tsx
+++ app/login-page/page.tsx
@@ -1,13 +1,16 @@
 import { Separator } from "@/components/ui/separator";
 import OAuthLogin from "./oauth-login";
 import ManualLogin from "./manual-login";
+import { Marker, MarkerContent } from "@/components/ui/marker";
 
 export default function SignInPage(){
     return (<>
         <div className="flex justify-center align-items-center items-center min-h-screen">
             <div className="grid gap-1 w-100">
                 <ManualLogin />
-                <Separator className="my-4"/>
+                <Marker variant="separator" className="my-2">
+                    <MarkerContent>or continue with</MarkerContent>
+                </Marker>
                 <OAuthLogin />
             </div>
         </div>
